2852a2ec9af3e127a54203cdddb2dd1d06cf72d0
Ailin Tobin <ailintobin@gmail.com>
2020-06-15 10:13:41 +0000

    added secretkey to run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from flask import Flask, redirect, render_template, request, session, url_for
 
 
 app = Flask(__name__)
-app.secret_key =  "randomstring123"
+app.secret_key = os.getenv("SECRET", "randomstring123")
 messages = []
 
 
@@ -48,4 +48,4 @@ def send_message(username, message):
     return redirect("/" + username)'''
 
 
-app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)),debug=True)
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 5000)), debug=False)
